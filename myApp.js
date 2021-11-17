f29a1ad77906bca71fa73b6c7b6d996391f20511
Jonathan Hurtig <jonathanhurtig@gmail.com>
2021-11-17 17:16:30 +0100

    added logging to check error

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 require('express')
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGO_URI)
+console.log(mongoose.connect(process.env.MONGO_URI))
 
 
 let Person;
